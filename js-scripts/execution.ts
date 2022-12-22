d88c4af7d0e14ea74be589ee291810a51e625e59
sendra <polsendra@gmail.com>
2022-12-22 16:45:35 +0100

    fix: added emergency admin. Updated to ts

--- js-scripts/execution.ts
+++ js-scripts/execution.ts
@@ -1,30 +1,34 @@
 import "dotenv/config";
-import { providers } from "ethers";
+import { providers, ethers } from "ethers";
 
-import AaveV3EthereumInitialPayload from '../out/AaveV3EthereumInitialPayload.sol/AaveV3EthereumInitialPayload.json' assert {type: 'json'};
-import ACLManager from './abis/ACLManager.json' assert {type: 'json'};
+import AaveV3EthereumInitialPayload from '../out/AaveV3EthereumInitialPayload.sol/AaveV3EthereumInitialPayload.json';
+import ACLManager from './abis/ACLManager.json';
+
+import {ACL_MANAGER, ACL_ADMIN} from '../lib/aave-address-book/src/ts/AaveV3EthereumDraft';
+import {SHORT_EXECUTOR} from '../lib/aave-address-book/src/ts/AaveGovernanceV2';
 
 const provider = new providers.StaticJsonRpcProvider(
   process.env.TENDERLY_FORK_RPC
 );
 
-const SHORT_EXECUTOR = '0xEE56e2B3D491590B5b31738cC34d5232F378a8D5';
-const aclManager = '0xc2aaCf6553D20d1e9d78E365AAba8032af9c85b0';
-const aclAdmin = '0xEE56e2B3D491590B5b31738cC34d5232F378a8D5'
 const payloadAddress = '0x8DFDBeacB95445c8e6eBfEdA9A4d2E40DC9cC3e5';
 
 const executePayload = async () => {
   // add pool admin permissions to payload
   const aclManagerContract = new ethers.Contract(
-    aclManager,
-    ACLManager.abi,
-    provider.getSigner(aclAdmin)
+    ACL_MANAGER,
+    ACLManager,
+    provider.getSigner(ACL_ADMIN)
   );
-  const addListingAdminTx = await aclManagerContract.addAssetListingAdmin(payloadAddress);
-  await addListingAdminTx.wait();
+  // const addListingAdminTx = await aclManagerContract.addAssetListingAdmin(payloadAddress);
+  // await addListingAdminTx.wait();
 
   const addPoolAdminTx = await aclManagerContract.addPoolAdmin(payloadAddress);
   await addPoolAdminTx.wait();
+
+  const addEmergencyAdminTx = await aclManagerContract.addEmergencyAdmin(payloadAddress);
+  await addEmergencyAdminTx.wait();
+
 
   // call payload
   const payloadContract = new ethers.Contract(
