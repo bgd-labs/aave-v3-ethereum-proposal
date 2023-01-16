d0aefad153e71b6f15bf1bc2ed20684284b066ce
eboado <ebdmrr@gmail.com>
2023-01-16 10:54:07 +0100

    - Added rate strategies definition and script. - Re-organisation to configs lib

--- scripts/AaveV3EthereumActions.s.sol
+++ scripts/AaveV3EthereumActions.s.sol
@@ -4,7 +4,9 @@ pragma solidity ^0.8.0;
 import {Script} from 'forge-std/Script.sol';
 import {GenericV3ListingEngine, IGenericV3ListingEngine} from 'aave-helpers/v3-listing-engine/GenericV3ListingEngine.sol';
 import {AaveV3EthereumDraft} from 'aave-address-book/AaveV3EthereumDraft.sol';
+import {DefaultReserveInterestRateStrategy} from 'aave-v3-core/contracts/protocol/pool/DefaultReserveInterestRateStrategy.sol';
 import {AaveV3EthereumInitialPayload} from '../src/contracts/AaveV3EthereumInitialPayload.sol';
+import {AaveV3EthereumRateStrategies} from '../src/contracts/AaveV3EthereumConfigsLib.sol';
 
 contract DeployEngine is Script {
   function run() external {
@@ -26,10 +28,36 @@ contract DeployPayload is Script {
   function run() external {
     vm.startBroadcast();
     IGenericV3ListingEngine v3ListingEngine = IGenericV3ListingEngine(
-      0xc148f4a658105dAD8D1F139b00f99aB914CeDd54
+      0xc148f4a658105dAD8D1F139b00f99aB914CeDd54 // TODO replace with final one of v3 Ethereum
     );
 
     new AaveV3EthereumInitialPayload(v3ListingEngine);
+    vm.stopBroadcast();
+  }
+}
+
+contract DeployRateStrategies is Script {
+  function run() external {
+    vm.startBroadcast();
+
+    AaveV3EthereumRateStrategies.RateStrategyConfig[] memory configs = AaveV3EthereumRateStrategies
+      ._getAllConfigs();
+
+    for (uint256 i = 0; i < configs.length; i++) {
+      new DefaultReserveInterestRateStrategy(
+        AaveV3EthereumDraft.POOL_ADDRESSES_PROVIDER, // TODO replace with final v3 Ethereum
+        configs[i].optimalUsageRatio,
+        configs[i].baseVariableBorrowRate,
+        configs[i].variableRateSlope1,
+        configs[i].variableRateSlope2,
+        configs[i].stableRateSlope1,
+        configs[i].stableRateSlope2,
+        configs[i].baseStableRateOffset,
+        configs[i].stableRateExcessOffset,
+        configs[i].optimalStableToTotalDebtRatio
+      );
+    }
+
     vm.stopBroadcast();
   }
 }
---
+++ src/contracts/AaveV3EthereumConfigsLib.sol
@@ -0,0 +1,122 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {WadRayMath} from 'aave-v3-core/contracts/protocol/libraries/math/WadRayMath.sol';
+
+library AaveV3EthereumAssets {
+  address internal constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+  address internal constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+  address internal constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+  address internal constant WSTETH = 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0;
+  address internal constant WBTC = 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599;
+  address internal constant LINK = 0x514910771AF9Ca656af840dff83E8264EcF986CA;
+  address internal constant AAVE = 0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9;
+}
+
+library AaveV3EthereumPriceFeeds {
+  address internal constant USDC_USD = 0x8fFfFfd4AfB6115b954Bd326cbe7B4BA576818f6;
+  address internal constant DAI_USD = 0xAed0c38402a5d19df6E4c03F4E2DceD6e29c1ee9;
+  address internal constant ETH_USD = 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419;
+  address internal constant WSTETH_USD = address(0); // TODO
+  address internal constant WBTC_USD = address(0); // TODO
+  address internal constant LINK_USD = 0x2c1d072e956AFFC0D435Cb7AC38EF18d24d9127c;
+  address internal constant AAVE_USD = 0x547a514d5e3769680Ce22B2361c10Ea13619e8a9;
+}
+
+/** @notice Simple library containing the initial rate strategies on Aave v3 Ethereum
+ */
+library AaveV3EthereumRateStrategies {
+  struct RateStrategyConfig {
+    uint256 optimalUsageRatio;
+    uint256 baseVariableBorrowRate;
+    uint256 variableRateSlope1;
+    uint256 variableRateSlope2;
+    uint256 stableRateSlope1;
+    uint256 stableRateSlope2;
+    uint256 baseStableRateOffset;
+    uint256 stableRateExcessOffset;
+    uint256 optimalStableToTotalDebtRatio;
+  }
+
+  // For USDC
+  function _rateStablesOne() internal pure returns (RateStrategyConfig memory) {
+    return
+      RateStrategyConfig({
+        optimalUsageRatio: _bpsToRay(90_00),
+        baseVariableBorrowRate: 0,
+        variableRateSlope1: _bpsToRay(4_00),
+        variableRateSlope2: _bpsToRay(60_00),
+        stableRateSlope1: _bpsToRay(50),
+        stableRateSlope2: _bpsToRay(60_00),
+        baseStableRateOffset: _bpsToRay(1_00),
+        stableRateExcessOffset: _bpsToRay(8_00),
+        optimalStableToTotalDebtRatio: _bpsToRay(20_00)
+      });
+  }
+
+  // For DAI
+  function _rateStablesTwo() internal pure returns (RateStrategyConfig memory) {
+    return
+      RateStrategyConfig({
+        optimalUsageRatio: _bpsToRay(80_00),
+        baseVariableBorrowRate: 0,
+        variableRateSlope1: _bpsToRay(4_00),
+        variableRateSlope2: _bpsToRay(75_00),
+        stableRateSlope1: _bpsToRay(50),
+        stableRateSlope2: _bpsToRay(75_00),
+        baseStableRateOffset: _bpsToRay(1_00),
+        stableRateExcessOffset: _bpsToRay(8_00),
+        optimalStableToTotalDebtRatio: _bpsToRay(20_00)
+      });
+  }
+
+  // For LINK, WBTC (and AAVE, but not enabled to borrow)
+  function _rateVolatilesOne() internal pure returns (RateStrategyConfig memory) {
+    return
+      RateStrategyConfig({
+        optimalUsageRatio: _bpsToRay(45_00),
+        baseVariableBorrowRate: 0,
+        variableRateSlope1: _bpsToRay(7_00),
+        variableRateSlope2: _bpsToRay(30_00),
+        stableRateSlope1: _bpsToRay(7_00),
+        stableRateSlope2: _bpsToRay(30_00),
+        baseStableRateOffset: _bpsToRay(2_00),
+        stableRateExcessOffset: _bpsToRay(5_00),
+        optimalStableToTotalDebtRatio: _bpsToRay(20_00)
+      });
+  }
+
+  // For ETH and wstETH
+  function _rateEth() internal pure returns (RateStrategyConfig memory) {
+    return
+      RateStrategyConfig({
+        optimalUsageRatio: _bpsToRay(80_00),
+        baseVariableBorrowRate: 0,
+        variableRateSlope1: _bpsToRay(5_75),
+        variableRateSlope2: _bpsToRay(80_00),
+        stableRateSlope1: _bpsToRay(4_00),
+        stableRateSlope2: _bpsToRay(80_00),
+        baseStableRateOffset: _bpsToRay(3_00),
+        stableRateExcessOffset: _bpsToRay(5_00),
+        optimalStableToTotalDebtRatio: _bpsToRay(20_00)
+      });
+  }
+
+  function _getAllConfigs() internal pure returns (RateStrategyConfig[] memory) {
+    RateStrategyConfig[] memory configs = new RateStrategyConfig[](4);
+
+    configs[0] = _rateStablesOne();
+    configs[1] = _rateStablesTwo();
+    configs[2] = _rateVolatilesOne();
+    configs[3] = _rateEth();
+
+    return configs;
+  }
+
+  /** @dev Converts basis points to RAY units
+   * e.g. 10_00 (10.00%) will return 100000000000000000000000000
+   */
+  function _bpsToRay(uint256 amount) internal pure returns (uint256) {
+    return (amount * WadRayMath.RAY) / 10_000;
+  }
+}
