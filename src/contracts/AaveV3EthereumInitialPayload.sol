366afb7589dab30877194809e99e3417d36436c4
eboado <ebdmrr@gmail.com>
2023-01-17 15:53:07 +0100

    - Added address of deployed v3 listing engine on scripts. - Modified scripts to deploy only the missing WETH and wstETH rate strategies. - Updated rate strategy definitions for WETH and wstETH. - Updated v3 Ethereum payload

--- src/contracts/AaveV3EthereumInitialPayload.sol
+++ src/contracts/AaveV3EthereumInitialPayload.sol
@@ -1,31 +1,85 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {AaveV3EthereumDraft} from 'aave-address-book/AaveV3EthereumDraft.sol';
+import {AaveV3Ethereum} from 'aave-address-book/AaveV3Ethereum.sol';
+import {AaveGovernanceV2} from 'aave-address-book/AaveGovernanceV2.sol';
 import {AaveV3ListingEthereum, IGenericV3ListingEngine} from 'aave-helpers/v3-listing-engine/AaveV3ListingEthereum.sol';
+
+library AaveV3EthereumAssets {
+  address internal constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+  address internal constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+  address internal constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+  address internal constant WSTETH = 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0;
+  address internal constant WBTC = 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599;
+  address internal constant LINK = 0x514910771AF9Ca656af840dff83E8264EcF986CA;
+  address internal constant AAVE = 0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9;
+}
+
+library AaveV3EthereumPriceFeeds {
+  address internal constant USDC_USD = 0x8fFfFfd4AfB6115b954Bd326cbe7B4BA576818f6;
+  address internal constant DAI_USD = 0xAed0c38402a5d19df6E4c03F4E2DceD6e29c1ee9;
+  address internal constant ETH_USD = 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419;
+  address internal constant WSTETH_USD = 0xA9F30e6ED4098e9439B2ac8aEA2d3fc26BcEbb45;
+  address internal constant WBTC_USD = 0x230E0321Cf38F09e247e50Afc7801EA2351fe56F;
+  address internal constant LINK_USD = 0x2c1d072e956AFFC0D435Cb7AC38EF18d24d9127c;
+  address internal constant AAVE_USD = 0x547a514d5e3769680Ce22B2361c10Ea13619e8a9;
+}
+
+library AaveV3EthereumRateStrategies {
+  address internal constant USDC = 0xD6293edBB2E5E0687a79F01BEcd51A778d59D1c5;
+  address internal constant DAI = 0x694d4cFdaeE639239df949b6E24Ff8576A00d1f2;
+  address internal constant WETH = 0x165e90Bd0a41d08fA1891CcDCEe315D7b83B3419;
+  address internal constant WSTETH = 0x7b8Fa4540246554e77FCFf140f9114de00F8bB8D;
+  address internal constant WBTC = 0x24701A6368Ff6D2874d6b8cDadd461552B8A5283;
+  address internal constant LINK = 0x24701A6368Ff6D2874d6b8cDadd461552B8A5283;
+  address internal constant AAVE = 0x24701A6368Ff6D2874d6b8cDadd461552B8A5283;
+}
 
 /**
  * @notice AaveV3EthereumInitialPayload
  * @dev Initial payload of Aave v3 Ethereum:
  * - Unpauses the Aave v3 Ethereum pool
- * - Lists the initial assets decided by the Aave community
+ * - Creates eMode for ETH-correlated
+ * - Lists the initial assets decided by the Aave community (USDC, DAI, WETH, wstETH, WBTC, AAVE and LINK)
+ * - Adds the Aave Guardian as EMERGENCY_ADMIN and revokes the Governance Short Executor (Level 1)
  * @author BGD Labs
  */
 contract AaveV3EthereumInitialPayload is AaveV3ListingEthereum {
+  uint8 public constant EMODE_ID_ETH_CORRELATED = uint8(1);
+  string public constant EMODE_LABEL_ETH_CORRELATED = 'ETH correlated';
+  uint16 public constant EMODE_LTV_ETH_CORRELATED = 90_00;
+  uint16 public constant EMODE_LT_ETH_CORRELATED = 93_00;
+  uint16 public constant EMODE_LBONUS_ETH_CORRELATED = 10_100;
+
+  address public constant GUARDIAN_ETHEREUM = 0xCA76Ebd8617a03126B6FB84F9b1c1A0fB71C2633;
+
   constructor(IGenericV3ListingEngine listingEngine) AaveV3ListingEthereum(listingEngine) {}
 
   function _preExecute() internal override {
-    AaveV3EthereumDraft.POOL_CONFIGURATOR.setPoolPause(false);
+    AaveV3Ethereum.POOL_CONFIGURATOR.setPoolPause(false);
+    AaveV3Ethereum.POOL_CONFIGURATOR.setEModeCategory(
+      EMODE_ID_ETH_CORRELATED,
+      EMODE_LTV_ETH_CORRELATED,
+      EMODE_LT_ETH_CORRELATED,
+      EMODE_LBONUS_ETH_CORRELATED,
+      address(0),
+      EMODE_LABEL_ETH_CORRELATED
+    );
+
+    // Same as using 'address(this)' in the context of SHORT_EXECUTOR delegatecall, but seems more correct
+    // to use aave-address-book to avoid assumptions
+    AaveV3Ethereum.ACL_MANAGER.removeEmergencyAdmin(AaveGovernanceV2.SHORT_EXECUTOR);
+    AaveV3Ethereum.ACL_MANAGER.addEmergencyAdmin(GUARDIAN_ETHEREUM);
   }
 
   function getAllConfigs() public pure override returns (IGenericV3ListingEngine.Listing[] memory) {
-    IGenericV3ListingEngine.Listing[] memory listings = new IGenericV3ListingEngine.Listing[](4);
+    IGenericV3ListingEngine.Listing[] memory listings = new IGenericV3ListingEngine.Listing[](7);
 
     listings[0] = IGenericV3ListingEngine.Listing({
-      asset: 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599,
+      asset: AaveV3EthereumAssets.WBTC,
       assetSymbol: 'WBTC',
-      priceFeed: 0xF4030086522a5bEEa4988F8cA5B36dbC97BeE88c, // TODO to change to WBTC based
-      rateStrategy: 0xC7Da397f2AaB3795427D6e9d602BB27B49c57f1b,
+      priceFeed: AaveV3EthereumPriceFeeds.WBTC_USD,
+      rateStrategy: AaveV3EthereumRateStrategies.WBTC,
       enabledToBorrow: true,
       stableRateModeEnabled: false,
       borrowableInIsolation: false,
@@ -34,18 +88,18 @@ contract AaveV3EthereumInitialPayload is AaveV3ListingEthereum {
       ltv: 70_00,
       liqThreshold: 75_00,
       liqBonus: 6_25,
-      reserveFactor: 10_00,
+      reserveFactor: 20_00,
       supplyCap: 43_000,
-      borrowCap: 22_000,
+      borrowCap: 28_000,
       debtCeiling: 0,
       liqProtocolFee: 10_00,
       eModeCategory: 0
     });
     listings[1] = IGenericV3ListingEngine.Listing({
-      asset: 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2,
+      asset: AaveV3EthereumAssets.WETH,
       assetSymbol: 'WETH',
-      priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419,
-      rateStrategy: 0xC7Da397f2AaB3795427D6e9d602BB27B49c57f1b,
+      priceFeed: AaveV3EthereumPriceFeeds.ETH_USD,
+      rateStrategy: AaveV3EthereumRateStrategies.WETH,
       enabledToBorrow: true,
       stableRateModeEnabled: false,
       borrowableInIsolation: false,
@@ -54,18 +108,38 @@ contract AaveV3EthereumInitialPayload is AaveV3ListingEthereum {
       ltv: 80_00,
       liqThreshold: 82_50,
       liqBonus: 5_00,
-      reserveFactor: 10_00,
+      reserveFactor: 15_00,
       supplyCap: 1_800_000,
-      borrowCap: 990_000,
+      borrowCap: 1_400_000,
       debtCeiling: 0,
       liqProtocolFee: 10_00,
-      eModeCategory: 0
+      eModeCategory: EMODE_ID_ETH_CORRELATED
     });
     listings[2] = IGenericV3ListingEngine.Listing({
-      asset: 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48,
+      asset: AaveV3EthereumAssets.WSTETH,
+      assetSymbol: 'wstETH',
+      priceFeed: AaveV3EthereumPriceFeeds.WSTETH_USD,
+      rateStrategy: AaveV3EthereumRateStrategies.WSTETH,
+      enabledToBorrow: true,
+      stableRateModeEnabled: false,
+      borrowableInIsolation: false,
+      withSiloedBorrowing: false,
+      flashloanable: true,
+      ltv: 68_50,
+      liqThreshold: 79_50,
+      liqBonus: 7_00,
+      reserveFactor: 15_00,
+      supplyCap: 200_000,
+      borrowCap: 3_000,
+      debtCeiling: 0,
+      liqProtocolFee: 10_00,
+      eModeCategory: EMODE_ID_ETH_CORRELATED
+    });
+    listings[3] = IGenericV3ListingEngine.Listing({
+      asset: AaveV3EthereumAssets.USDC,
       assetSymbol: 'USDC',
-      priceFeed: 0x8fFfFfd4AfB6115b954Bd326cbe7B4BA576818f6,
-      rateStrategy: 0x24701A6368Ff6D2874d6b8cDadd461552B8A5283,
+      priceFeed: AaveV3EthereumPriceFeeds.USDC_USD,
+      rateStrategy: AaveV3EthereumRateStrategies.USDC,
       enabledToBorrow: true,
       stableRateModeEnabled: false,
       borrowableInIsolation: false,
@@ -75,17 +149,17 @@ contract AaveV3EthereumInitialPayload is AaveV3ListingEthereum {
       liqThreshold: 76_00,
       liqBonus: 4_50,
       reserveFactor: 10_00,
-      supplyCap: 678_000,
-      borrowCap: 373_000,
+      supplyCap: 1_760_000_000,
+      borrowCap: 1_580_000_000,
       debtCeiling: 0,
-      liqProtocolFee: 10_00,
+      liqProtocolFee: 20_00,
       eModeCategory: 0
     });
-    listings[3] = IGenericV3ListingEngine.Listing({
-      asset: 0x6B175474E89094C44Da98b954EedeAC495271d0F,
+    listings[4] = IGenericV3ListingEngine.Listing({
+      asset: AaveV3EthereumAssets.DAI,
       assetSymbol: 'DAI',
-      priceFeed: 0xAed0c38402a5d19df6E4c03F4E2DceD6e29c1ee9,
-      rateStrategy: 0x24701A6368Ff6D2874d6b8cDadd461552B8A5283,
+      priceFeed: AaveV3EthereumPriceFeeds.DAI_USD,
+      rateStrategy: AaveV3EthereumRateStrategies.DAI,
       enabledToBorrow: true,
       stableRateModeEnabled: false,
       borrowableInIsolation: false,
@@ -95,8 +169,50 @@ contract AaveV3EthereumInitialPayload is AaveV3ListingEthereum {
       liqThreshold: 77_00,
       liqBonus: 4_00,
       reserveFactor: 10_00,
-      supplyCap: 103_000,
-      borrowCap: 57_000,
+      supplyCap: 338_000_000,
+      borrowCap: 271_000_000,
+      debtCeiling: 0,
+      liqProtocolFee: 20_00,
+      eModeCategory: 0
+    });
+
+    listings[5] = IGenericV3ListingEngine.Listing({
+      asset: AaveV3EthereumAssets.LINK,
+      assetSymbol: 'LINK',
+      priceFeed: AaveV3EthereumPriceFeeds.LINK_USD,
+      rateStrategy: AaveV3EthereumRateStrategies.LINK,
+      enabledToBorrow: true,
+      stableRateModeEnabled: false,
+      borrowableInIsolation: false,
+      withSiloedBorrowing: false,
+      flashloanable: true,
+      ltv: 50_00,
+      liqThreshold: 65_00,
+      liqBonus: 7_50,
+      reserveFactor: 20_00,
+      supplyCap: 24_000_000,
+      borrowCap: 13_000_000,
+      debtCeiling: 0,
+      liqProtocolFee: 10_00,
+      eModeCategory: 0
+    });
+
+    listings[6] = IGenericV3ListingEngine.Listing({
+      asset: AaveV3EthereumAssets.AAVE,
+      assetSymbol: 'AAVE',
+      priceFeed: AaveV3EthereumPriceFeeds.AAVE_USD,
+      rateStrategy: AaveV3EthereumRateStrategies.AAVE,
+      enabledToBorrow: false,
+      stableRateModeEnabled: false,
+      borrowableInIsolation: false,
+      withSiloedBorrowing: false,
+      flashloanable: false,
+      ltv: 60_00,
+      liqThreshold: 70_00,
+      liqBonus: 7_50,
+      reserveFactor: 0,
+      supplyCap: 1_850_000,
+      borrowCap: 0,
       debtCeiling: 0,
       liqProtocolFee: 10_00,
       eModeCategory: 0
