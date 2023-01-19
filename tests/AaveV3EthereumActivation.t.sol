6ecd26777a8db8f1fc810305b5022a828433f991
Lukas <lukasstrassel@googlemail.com>
2023-01-19 12:36:09 +0100

    feat: update to use 3_0_1 test

--- tests/AaveV3EthereumActivation.t.sol
+++ tests/AaveV3EthereumActivation.t.sol
@@ -3,13 +3,13 @@ pragma solidity ^0.8.0;
 
 import 'forge-std/Test.sol';
 import {AaveV3Ethereum} from 'aave-address-book/AaveAddressBook.sol';
-import {ProtocolV3TestBase, ReserveConfig} from 'aave-helpers/ProtocolV3TestBase.sol';
+import {ProtocolV3_0_1TestBase, ReserveConfig} from 'aave-helpers/ProtocolV3TestBase.sol';
 import {AaveGovernanceV2} from 'aave-address-book/AaveGovernanceV2.sol';
 import {IGenericV3ListingEngine} from 'aave-helpers/v3-listing-engine/GenericV3ListingEngine.sol';
 import {AaveV3EthereumInitialPayload} from '../src/contracts/AaveV3EthereumInitialPayload.sol';
 import {BaseTest} from './utils/BaseTest.sol';
 
-contract AaveV3EthereumActivation is ProtocolV3TestBase, BaseTest {
+contract AaveV3EthereumActivation is ProtocolV3_0_1TestBase, BaseTest {
   // TODO when final, this should go to address-book
   IGenericV3ListingEngine V3_ETHEREUM_LISTING_ENGINE =
     IGenericV3ListingEngine(0xC51e6E38d406F98049622Ca54a6096a23826B426);
