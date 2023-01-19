c2b41bad8c658fbcfcfc17eafa0adcfa9590ab2b
Lukas <lukasstrassel@googlemail.com>
2023-01-20 00:55:08 +0100

    fix: update helpers & address book

--- tests/AaveV3EthereumActivation.t.sol
+++ tests/AaveV3EthereumActivation.t.sol
@@ -6,17 +6,16 @@ import {AaveV3Ethereum} from 'aave-address-book/AaveAddressBook.sol';
 import {ProtocolV3_0_1TestBase, ReserveConfig} from 'aave-helpers/ProtocolV3TestBase.sol';
 import {AaveGovernanceV2} from 'aave-address-book/AaveGovernanceV2.sol';
 import {IGenericV3ListingEngine} from 'aave-helpers/v3-listing-engine/GenericV3ListingEngine.sol';
+import {TestWithExecutor} from 'aave-helpers/GovHelpers.sol';
 import {AaveV3EthereumInitialPayload} from '../src/contracts/AaveV3EthereumInitialPayload.sol';
-import {BaseTest} from './utils/BaseTest.sol';
 
-contract AaveV3EthereumActivation is ProtocolV3_0_1TestBase, BaseTest {
-  // TODO when final, this should go to address-book
+contract AaveV3EthereumActivation is ProtocolV3_0_1TestBase, TestWithExecutor {
   IGenericV3ListingEngine V3_ETHEREUM_LISTING_ENGINE =
-    IGenericV3ListingEngine(0xC51e6E38d406F98049622Ca54a6096a23826B426);
+    IGenericV3ListingEngine(AaveV3Ethereum.LISTING_ENGINE);
 
   function setUp() public {
     vm.createSelectFork(vm.rpcUrl('mainnet'), 16434920);
-    _setUp(AaveGovernanceV2.SHORT_EXECUTOR);
+    _selectPayloadExecutor(AaveGovernanceV2.SHORT_EXECUTOR);
   }
 
   function testPoolActivation() public {
@@ -26,7 +25,7 @@ contract AaveV3EthereumActivation is ProtocolV3_0_1TestBase, BaseTest {
       new AaveV3EthereumInitialPayload(V3_ETHEREUM_LISTING_ENGINE)
     );
 
-    _execute(activationPayload);
+    _executePayload(activationPayload);
 
     createConfigurationSnapshot('post-aave-v3-ethereum', AaveV3Ethereum.POOL);
   }
